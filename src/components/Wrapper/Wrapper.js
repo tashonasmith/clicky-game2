97ff4a5a3811c11a35dc368803b80d7f0a5ab859
Tashona Smith <tashona.smith@gmail.com>
2020-07-21 01:41:02 -0500

    Updates

--- src/components/Wrapper/Wrapper.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import styles from "./Wrapper.module.css"
-
-
-const Wrapper = props => <div className={styles.wrapper}>{props.children}</div>;
-
-export default Wrapper;
-
-
