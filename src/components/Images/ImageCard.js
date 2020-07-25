7f177a2895faf464568ddda92aa93b3f8a7af846
Tashona Smith <tashona.smith@gmail.com>
2020-07-24 21:17:23 -0500

    Adjust image styling

--- src/components/Images/ImageCard.js
+++ src/components/Images/ImageCard.js
@@ -1,13 +1,16 @@
 import React from "react";
 import styles from "./ImageCard.module.css";
 
-const ImageCard = props => (
-  <div className={styles.card} value={props.id} onClick={() => props.handleClick(props.id)}>
-    <div className={styles.container}>
-      <img className={styles.photo} alt={props.name} src={props.image} />
-    </div>
+const ImageCard = (props) => (
+  <div
+    className={styles.card}
+    value={props.id}
+    onClick={() => props.handleClick(props.id)}>
+    <div
+      className={styles.photo}
+      style={{ backgroundImage: `url('${props.image}')` }}></div>
   </div>
-)
+);
 
 export default ImageCard
 
